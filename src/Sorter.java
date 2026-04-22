ded511765bfd32d743a7c4eac4da49b1bef69785
arabdrakh <nurasyloran@gmail.com>
2026-04-22 08:49:50 +0500

    feat(sorter): added advanced sorting algorithm Merge sortt

---
+++ src/Sorter.java
@@ -0,0 +1,57 @@
+import java.util.Arrays;
+public class Sorter {
+    //aruzhan abd it-2501
+    //insertion sort
+    public void basicSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+    //Aruzhan Abdrakhmanova IT-2501
+    public void advancedSort(int[] arr) {
+        if (arr.length < 2) return;
+        mergeSort(arr, 0, arr.length - 1);
+    }
+
+    private void mergeSort(int[] arr, int left, int right) {
+        if (left >= right) return;
+
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+
+    private void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;    // size of left sub-array
+        int n2 = right - mid;       // size of right sub-array
+
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+
+        System.arraycopy(arr, left, leftArr, 0, n1);
+        System.arraycopy(arr, mid + 1, rightArr, 0, n2);
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k++] = leftArr[i++];
+            } else {
+                arr[k++] = rightArr[j++];
+            }
+        }
+
+        while (i < n1) arr[k++] = leftArr[i++];
+
+        while (j < n2) arr[k++] = rightArr[j++];
+    }
+}
